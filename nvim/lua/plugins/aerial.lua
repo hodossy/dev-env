f1cffcda15fbee874189b874737fb85d8f1049a0
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-04-24 15:44:33 +0200

    feat: add nx and aerial plugins

---
+++ nvim/lua/plugins/aerial.lua
@@ -0,0 +1,12 @@
+return {
+  'stevearc/aerial.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = {
+     "nvim-treesitter/nvim-treesitter",
+     "nvim-tree/nvim-web-devicons"
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+  end
+}
