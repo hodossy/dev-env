e81ba23f0ee5dbdd11fd8a249ca68a43c4cc45d6
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-04-26 10:26:35 +0200

    fix: properly set up aerial

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -7,6 +7,10 @@ return {
      "nvim-tree/nvim-web-devicons"
   },
   config = function()
+    require('aerial').setup({
+      backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" }
+    })
+
     vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
-  end
+  end,
 }
