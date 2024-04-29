f83fbf495be1655f05b93bcc4378df5e4488bee9
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-04-29 13:07:13 +0200

    feat: add catppuccin integrations

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,16 @@ return {
   lazy = false,
   name = "catppuccin",
   priority = 1000,
+  opts = {
+    integrations = {
+      aerial = true,
+      mason = true,
+      neotree = true,
+      telescope = true,
+      treesitter = true,
+      which_key = true,
+    },
+  },
   config = function()
     vim.cmd.colorscheme "catppuccin-mocha"
   end
