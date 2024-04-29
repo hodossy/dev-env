bfe5939afb2da0b1746a9c3ceadba4d064b125ea
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-04-29 13:06:42 +0200

    feat: change nvim pane navigation

--- nvim/lua/user/opts.lua
+++ nvim/lua/user/opts.lua
@@ -11,3 +11,9 @@ vim.opt.expandtab = true
 
 -- Editor settings
 vim.opt.number = true
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
