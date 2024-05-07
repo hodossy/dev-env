94c6d25a2f33760b23881aa0a79177fb994c4108
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-05-07 14:56:58 +0200

    fix: conflicting mini.move mappings

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,23 +1,40 @@
 return {
-  {
-    "echasnovski/mini.comment",
-    opts = {
-      options = {
-        custom_commentstring = function()
-          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-        end,
-      },
-    },
-  },
-  {
-    "echasnovski/mini.move",
-  },
-  {
-    "echasnovski/mini.pairs",
-    opts = {
-      mappings = {
-        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
-      },
-    },
-  }
+	{
+		"echasnovski/mini.comment",
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
+		},
+	},
+	{
+		"echasnovski/mini.move",
+		opts = {
+			-- Module mappings. Use `''` (empty string) to disable one.
+			mappings = {
+				-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+				left = "<M-Left>",
+				right = "<M-Right>",
+				down = "<M-Down>",
+				up = "<M-Up>",
+
+				-- Move current line in Normal mode
+				line_left = "<M-Left>",
+				line_right = "<M-Right>",
+				line_down = "<M-Down>",
+				line_up = "<M-Up>",
+			},
+		},
+	},
+	{
+		"echasnovski/mini.pairs",
+		opts = {
+			mappings = {
+				["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
+			},
+		},
+	},
 }
