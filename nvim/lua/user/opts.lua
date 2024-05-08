ad287fe5dc4607637319d4e9d5462b897cf2f368
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-05-08 13:15:34 +0200

    feat: add hybrid line numbers

--- nvim/lua/user/opts.lua
+++ nvim/lua/user/opts.lua
@@ -11,9 +11,11 @@ vim.opt.expandtab = true
 
 -- Editor settings
 vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
