28b0e8c866ee34783302ea60bcc99d128a8a3484
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-05-15 15:38:52 +0200

    feat: add fuzzy search to telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-telescope/telescope-ui-select.nvim",
   },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
@@ -19,6 +20,13 @@ return {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          },
         },
       })
       local builtin = require("telescope.builtin")
@@ -27,6 +35,7 @@ return {
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 
       require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("fzf")
     end,
   },
 }
