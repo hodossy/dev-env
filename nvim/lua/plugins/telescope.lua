2863471156e052ec98f13039f676dc2e3933790a
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-05-29 14:45:26 +0200

    feat: add fuzzy search within buffer

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,41 +1,47 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          layout_config = {
-            horizontal = {
-              preview_cutoff = 0,
-            },
-          },
-        },
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-          fzf = {
-            fuzzy = true,
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-            -- the default case_mode is "smart_case"
-          },
-        },
-      })
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files (fuzzy)" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find files by content" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					layout_config = {
+						horizontal = {
+							preview_cutoff = 0,
+						},
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+						-- the default case_mode is "smart_case"
+					},
+				},
+			})
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files (fuzzy)" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find files by content" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+			vim.keymap.set(
+				"n",
+				"<leader>fe",
+				builtin.current_buffer_fuzzy_find,
+				{ desc = "Find in current buffer (fuzzy)" }
+			)
 
-      require("telescope").load_extension("ui-select")
-      require("telescope").load_extension("fzf")
-    end,
-  },
+			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("fzf")
+		end,
+	},
 }
