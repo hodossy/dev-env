1ebd7a3afbc2f050d3bfa807bbd887cabb614b00
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-05-29 21:37:10 +0200

    feat: add line blame and diff view toggle

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,24 @@
 return {
-  'lewis6991/gitsigns.nvim',
-  config = function()
-    require('gitsigns').setup()
-  end,
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 200,
+			},
+		})
+
+		vim.keymap.set(
+			"n",
+			"<leader>gtb",
+			":Gitsigns toggle_current_line_blame<CR>",
+			{ desc = "Toggle current line blame" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>gtd",
+			":Gitsigns toggle_deleted<CR>:Gitsigns toggle_linehl<CR>",
+			{ desc = "Toggle diff view" }
+		)
+	end, -- what?
 }
--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -1,8 +1,8 @@
 -- Navigate vim panes better
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>", { desc = "Select window to the top" })
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>", { desc = "Select window to the bottom" })
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>", { desc = "Select window to the left" })
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>", { desc = "Select window to the right" })
 
 -- clear highlight with Esc
-vim.keymap.set("n", "<Esc>", ":let @/=''<CR>")
+vim.keymap.set("n", "<Esc>", ":let @/=''<CR>", { desc = "Clear highlight" })
