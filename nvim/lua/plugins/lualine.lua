f3de5cb71bc51e6eadbe5cb37e3b5ddb61a565bb
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-06-12 14:52:42 +0200

    feat: customize lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,35 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'codedark',
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "codedark",
+			},
+			sections = {
+				lualine_c = {
+					{ "filename", path = 1 },
+				},
+				lualine_x = {
+					"encoding",
+					{
+						"fileformat",
+						symbols = {
+							unix = "LF", -- '', -- e712
+							dos = "CRLF", -- '',  -- e70f
+							mac = "CR", -- '',  -- e711
+						},
+					},
+					"filetype",
+				},
+			},
+			extensions = {
+				"aerial",
+				"lazy",
+				"mason",
+				"neo-tree",
+				"toggleterm",
+			},
+		})
+	end,
 }
--- nvim/lua/plugins/scratch.lua
+++ nvim/lua/plugins/scratch.lua
@@ -1,15 +1,15 @@
 return {
-  "swaits/scratch.nvim",
-  lazy = true,
-  keys = {
-    { "<leader>bs", "<cmd>Scratch<cr>",      desc = "Scratch Buffer",         mode = "n" },
-    { "<leader>bS", "<cmd>ScratchSplit<cr>", desc = "Scratch Buffer (split)", mode = "n" },
-  },
-  cmd = {
-    "Scratch",
-    "ScratchSplit",
-  },
-  opts = {
-    buffer_name = "Scratch",
-  },
+	"swaits/scratch.nvim",
+	lazy = true,
+	keys = {
+		{ "<leader>bs", "<cmd>Scratch<cr>", desc = "Scratch Buffer", mode = "n" },
+		{ "<leader>bS", "<cmd>ScratchSplit<cr>", desc = "Scratch Buffer (split)", mode = "n" },
+	},
+	cmd = {
+		"Scratch",
+		"ScratchSplit",
+	},
+	opts = {
+		buffer_name = "Scratch",
+	},
 }
