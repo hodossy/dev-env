77f763e8d82a803265d3c13f9370980a36784a1a
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-11-26 14:14:33 +0100

    feat(nvim): tweak trouble keymaps and layout
    Signed-off-by: Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,15 +1,22 @@
 return {
   "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  -- for default options, refer to the configuration section for custom setup.
+  opts = {
+    auto_close = true,
+    focus = true,
+    win = {
+      type = "float",
+    },
+  },
   cmd = "Trouble",
   keys = {
     {
-      "<leader>xx",
+      "<leader>xX",
       "<cmd>Trouble diagnostics toggle<cr>",
       desc = "Diagnostics (Trouble)",
     },
     {
-      "<leader>xX",
+      "<leader>xx",
       "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
       desc = "Buffer Diagnostics (Trouble)",
     },
