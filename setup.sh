5f14242e01a03201b1afe707abb1acd965d368a1
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2024-11-27 10:26:33 +0100

    feat: enable selective tool install
    Signed-off-by: Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>

--- setup.sh
+++ setup.sh
@@ -2,8 +2,25 @@
 
 set -e  # exit on error
 
-for f in scripts/setup_*.sh scripts/**/setup_*.sh; do
-  if [[ ! "$f" =~ "*" ]]; then
-    bash "$f"
-  fi
-done
+if [[ $# -ne 0 ]]; then
+  for tool in $@; do
+    for f in scripts/setup_$tool.sh; do
+      if [[ ! "$f" =~ "*" ]]; then
+        bash "$f"
+      fi
+    done
+  done
+  for tool in $@; do
+    for f in scripts/**/setup_$tool.sh; do
+      if [[ ! "$f" =~ "*" ]]; then
+        bash "$f"
+      fi
+    done
+  done
+else
+  for f in scripts/setup_*.sh scripts/**/setup_*.sh; do
+    if [[ ! "$f" =~ "*" ]]; then
+      bash "$f"
+    fi
+  done
+fi
