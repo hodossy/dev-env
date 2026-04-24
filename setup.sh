d7ea0b4b32676d8a293c943402a133bff2822e71
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2026-04-24 10:37:13 +0200

    refact(core): use mise for tool install
    Signed-off-by: Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>

--- setup.sh
+++ setup.sh
@@ -2,25 +2,23 @@
 
 set -e  # exit on error
 
-if [[ $# -ne 0 ]]; then
-  for tool in $@; do
-    for f in scripts/setup_$tool.sh; do
-      if [[ ! "$f" =~ "*" ]]; then
-        bash "$f"
-      fi
-    done
-  done
-  for tool in $@; do
-    for f in scripts/**/setup_$tool.sh; do
-      if [[ ! "$f" =~ "*" ]]; then
-        bash "$f"
-      fi
-    done
-  done
+error() {
+  echo "$@" >&2
+  exit 1
+}
+
+# Install Mise
+os="$(uname -s)"
+if [ "$os" = Linux ]; then
+  gpg --keyserver hkps://keys.openpgp.org --recv-keys 24853EC9F655CE80B48E6C3A8B81C9D17413A06D
+  curl https://mise.jdx.dev/install.sh.sig | gpg --decrypt | sh
 else
-  for f in scripts/setup_*.sh scripts/**/setup_*.sh; do
-    if [[ ! "$f" =~ "*" ]]; then
-      bash "$f"
-    fi
-  done
+  error "unsupported OS: $os"
+fi
+
+# Configure all tools at once
+if [ ! -d ~/.config ]; then
+  ln -s `pwd`/config ~/.config
+else
+  error "$HOME/.config already exists, skipping tool configuration"
 fi
