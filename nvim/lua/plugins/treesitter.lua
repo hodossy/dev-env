9a3698510d90ec1fcabda3944820c2f2975e302a
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2026-04-24 10:24:45 +0200

    chore(nvim): update plugins
    Signed-off-by: Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,8 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
 		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ensure_installed = { "lua", "javascript", "html", "css", "python" },
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
